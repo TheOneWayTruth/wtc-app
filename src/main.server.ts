7e673489c50cbb46019665fd306fab6427f2eee8
theonewaytruth <sandra.werman@googlemail.com>
2023-02-06 15:25:32 +0100

    server.ts

---
+++ src/main.server.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env["PORT"] || 4201;
+
+
+app.get("/", (req: any, res: any ) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => console.log("Server listening at port 3000"));
