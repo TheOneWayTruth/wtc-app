a72b51ac85c39e95e7a6755ce20d07b328e04e45
CptVoltek <a.w.voltek@gmail.com>
2023-02-07 08:22:25 +0100

    rest

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/all", (req, res) => {
   res.send("Hello World");
 });
 
