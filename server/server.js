14cb2bf80c1ef9012b01ff8133a42f52c481ac00
theonewaytruth <sandra.werman@googlemail.com>
2023-02-07 08:54:23 +0100

    serer

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+var privateKey = fs.readFileSync("server.key", "utf8");
+var certificate = fs.readFileSync("server.crt", "utf8");
+
+var credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: "tbhhsy78",
+};
 
 app.get("/all", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log("Server listening at port 3000"));
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
+  console.log("server starting on port : " + PORT);
+});
